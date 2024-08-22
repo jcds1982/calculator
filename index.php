f0bdacfbd8f2af737b1a7482760d9e1c2e060420
jcds1982 <jcds1982@gmail.com>
2024-08-22 08:26:11 -0400

    Updating calculator width

--- index.php
+++ index.php
@@ -2,9 +2,6 @@
 //session_start();
 //$_SESSION['path'] = getcwd();
 //$_SESSION['path'] = str_replace('\\', '/', $_SESSION['path']);
-require_once ("view/header.php");
+require_once("view/header.php");
 require_once("view/calculator_Mainpanel.php");
-require_once ("view/footer.php");
-
-
-
+require_once("view/footer.php");
